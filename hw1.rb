bead35ed26012303e5b3dd3d52c849350771e05a
Rupert Kay <rupertkay96@gmail.com>
2018-02-15 18:20:44 -0800

    new hw1

--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,15 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  return (a+b)*(a+b);
 
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
+  a=a.sort
+  return a+1
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  return first_name + " " +last_name
 
 end
 
@@ -48,5 +48,5 @@ def scrabble(word)
     y: 4,
     z: 10,
   }
-  # Q5 CODE HERE
+  word.scan /\w/
 end
